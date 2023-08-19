08ec1006d553b76e93f7b58da0dfb3518af38e60
pksatyam777 <pksatyam777@gmail.com>
2023-08-19 12:26:32 +0530

    added header

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from 'next/image'
+import HomeHeader from './Components/HomeHeader'
 
 export default function Home() {
   return (
+    <>
+    <HomeHeader />
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        
       </div>
     </main>
+    </>
   )
 }
