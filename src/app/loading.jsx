003076234722aecfb17c959f6cf0921a0fa6919c
pksatyam777 <pksatyam777@gmail.com>
2023-08-20 20:16:23 +0530

    added search result and google api config

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,8 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function loading() {
   return (
-    <div>loading</div>
+    <div className="flex justify-center items-center mt-48"><Image className="bg-white" height="300" width="300" src="/loading.svg" alt="loading" /> </div>
   )
 }
