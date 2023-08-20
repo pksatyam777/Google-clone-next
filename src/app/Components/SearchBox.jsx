70b0e8679f40e33435e62f5f03dad3ed1d791bdb
pksatyam777 <pksatyam777@gmail.com>
2023-08-20 14:32:38 +0530

    added seach header

--- src/app/Components/SearchBox.jsx
+++ src/app/Components/SearchBox.jsx
@@ -1,21 +1,42 @@
 "use client"
 import React,{useState} from 'react'
 import {BsFillMicFill} from 'react-icons/bs'
+import { PiMagnifyingGlassThin } from "react-icons/pi";
+import {RxCross2} from 'react-icons/rx'
+import { useSearchParams, useRouter } from 'next/navigation';
+
 export default function SearchBox() {
-    const [input, setInput] = useState("");
+   
+    const params=useSearchParams()
+    const search = params.get('searchTerm')
+    const [input, setInput] = useState(search || '');
+    const redirect=useRouter()
+    const handleSubmit=(e)=>{
+      e.preventDefault();
+      if(!input.trim()) return;
+      redirect.push(`/search/web?searchTerm=${input}`)
+    }
   return (
     <form
         onSubmit={(e) => handleSubmit(e)}
-        className=" flex w-full m5-5 mx-auto max-w-[90%] my-4 border border-gray-200 items-center rounded-full px-2 py-3  hover:shadow-md transition-shadow focus-within:shadow-md sm:max-w-full lg:max-w-2xl"
-      >
-        
+        className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center"
+        >
+        {console.log(search)}
         <input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          className="flex-grow focus:outline-none"
+          className="flex-grow focus:outline-none px-2"
         />
-        <BsFillMicFill className="text-xl mx-3" />
+        
+          
+        <RxCross2 onClick={()=>setInput('')} className='text-xl  text-black cursor-pointer  ' />
+            
+            
+        <BsFillMicFill className="text-xl text-blue-600 mx-3 cursor-pointer hidden sm:inline-flex border-l-2 border-gray-300" />
+        <PiMagnifyingGlassThin className="text-xl text-blue-500 mx-3 cursor-pointer hidden sm:inline-flex" onClick={(e)=> handleSubmit(e)}/>
+          
+        
       </form>
   )
 }
--- src/app/Components/SearchHeader.jsx
+++ src/app/Components/SearchHeader.jsx
@@ -7,14 +7,14 @@ import { BsGrid3X3Gap } from "react-icons/bs";
 import SearchHeaderOptions from './SearchHeaderOptions'
 export default function SearchHeader() {
   return (
-    <header>
-      <div className="search-header flex justify-between  p-5 items-center">
+    <header className="sticky top-0 bg-white">
+      <div className="search-header flex w-full justify-between  p-6 items-center">
         <div className="">
           <Link href="/">
           <Image width="120" height="120" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png" />
           </Link>
         </div>
-        <div className="search-input">
+        <div className="search-input flex-1">
           <SearchBox />
         </div>
         <div className="nav-section  justify-between space-x-4 items-center hidden md:inline-flex">
